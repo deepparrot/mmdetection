016c280dc1e19a6b96357894dcee7a34d98b00eb
deepparrot <53057954+deepparrot@users.noreply.github.com>
2019-10-03 20:45:15 +0100

    Create sotabench_setup.sh

---
+++ sotabench_setup.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+source /workspace/venv/bin/activate
+PYTHON=${PYTHON:-"python"}
+
+pip install torch
+
+apt-get git
+git clone https://github.com/NVIDIA/apex
+cd apex
+pip install -v --no-cache-dir ./
+
+cd ..
+
+echo "Building roi align op..."
+cd mmdet/ops/roi_align
+if [ -d "build" ]; then
+    rm -r build
+fi
+$PYTHON setup.py build_ext --inplace
+
+echo "Building roi pool op..."
+cd ../roi_pool
+if [ -d "build" ]; then
+    rm -r build
+fi
+$PYTHON setup.py build_ext --inplace
+
+echo "Building nms op..."
+cd ../nms
+if [ -d "build" ]; then
+    rm -r build
+fi
+$PYTHON setup.py build_ext --inplace
+
+echo "Building dcn..."
+cd ../dcn
+if [ -d "build" ]; then
+    rm -r build
+fi
+$PYTHON setup.py build_ext --inplace
+
+echo "Building sigmoid focal loss op..."
+cd ../sigmoid_focal_loss
+if [ -d "build" ]; then
+    rm -r build
+fi
+$PYTHON setup.py build_ext --inplace
+
+echo "Building masked conv op..."
+cd ../masked_conv
+if [ -d "build" ]; then
+    rm -r build
+fi
+$PYTHON setup.py build_ext --inplace
