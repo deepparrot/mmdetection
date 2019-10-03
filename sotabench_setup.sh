9f596f2acff22749f2efc6650f4cf40c3844c7a7
deepparrot <53057954+deepparrot@users.noreply.github.com>
2019-10-03 21:13:54 +0100

    Update sotabench_setup.sh

--- sotabench_setup.sh
+++ sotabench_setup.sh
@@ -10,46 +10,5 @@ cd apex
 pip install -v --no-cache-dir ./
 
 cd ..
-ls
-echo "Building roi align op..."
-cd mmdet/ops/roi_align
-ls
-if [ -d "build" ]; then
-    rm -r build
-fi
-$PYTHON setup.py build_ext --inplace
-
-echo "Building roi pool op..."
-cd ../roi_pool
-if [ -d "build" ]; then
-    rm -r build
-fi
-$PYTHON setup.py build_ext --inplace
-
-echo "Building nms op..."
-cd ../nms
-if [ -d "build" ]; then
-    rm -r build
-fi
-$PYTHON setup.py build_ext --inplace
-
-echo "Building dcn..."
-cd ../dcn
-if [ -d "build" ]; then
-    rm -r build
-fi
-$PYTHON setup.py build_ext --inplace
-
-echo "Building sigmoid focal loss op..."
-cd ../sigmoid_focal_loss
-if [ -d "build" ]; then
-    rm -r build
-fi
-$PYTHON setup.py build_ext --inplace
-
-echo "Building masked conv op..."
-cd ../masked_conv
-if [ -d "build" ]; then
-    rm -r build
-fi
-$PYTHON setup.py build_ext --inplace
+echo "Installing..."
+$PYTHON setup.py develop
